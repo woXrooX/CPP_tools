b5846cfcdcfc84490d95777f756a514ac01f779f
woXrooX <woXrooX@gmail.com>
2025-08-15 11:56:35 +0500

    Sample pages

---
+++ source/web_server/Pages.h
@@ -0,0 +1,10 @@
+#ifndef WEB_SERVER_PAGES_H
+#define WEB_SERVER_PAGES_H
+
+namespace woXrooX{
+	Page home("home", "Home", false, false);
+	Page me("me", "My Page", true, true);
+	Page faq("FAQ", "Frequently Asked Question");
+}
+
+#endif
