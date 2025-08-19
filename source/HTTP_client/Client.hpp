cebbd18e58d1d630b7babf0891981c77949631e9
woXrooX <woXrooX@gmail.com>
2025-08-19 18:42:39 +0500

    Rename

--- source/HTTP_client/Client.hpp
+++ source/HTTP_client/Client.hpp
@@ -1,5 +1,5 @@
-#ifndef HTTP_CLIENT_H
-#define HTTP_CLIENT_H
+#ifndef HTTP_CLIENT_HPP
+#define HTTP_CLIENT_HPP
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,15 +8,15 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+
 namespace woXrooX{
 	class Client final{
 	public:
 		static void request(
 			const char* IP,
 			const int PORT,
-			const char* path,
-			const char* request_head,
-			const char* request_body
+			const char* path = "/",
+			const char* raw_request_data = ""
 		) {
 			Log::line();
 			Log::info("HTTP client is starting...");
@@ -28,7 +28,7 @@ namespace woXrooX{
 			Client::init_server_socket_address();
 			Client::create_socket_TCP();
 			Client::create_connect();
-			Client::out();
+			Client::out(raw_request_data);
 			Client::in();
 			Client::shutdown_socket_TCP();
 			Client::close_socket_TCP();
@@ -38,11 +38,11 @@ namespace woXrooX{
 		inline static const char* IP = nullptr;
 		inline static int PORT = 0;
 
+		inline static struct sockaddr_in server_socket_address;
+
 		inline static int server_socket_address_result = -1;
 		inline static int socket_TCP = -1;
 		inline static int connect_result = -1;
-
-		inline static struct sockaddr_in server_socket_address;
 
 		static void init_server_socket_address() {
 			// Zeroing addr before using
@@ -75,17 +75,32 @@ namespace woXrooX{
 			else Log::success("create_connect(): Success");
 		}
 
-		static void out() {
-			const int BUFFER_SIZE = 256;
-			char buffer[BUFFER_SIZE];
-			int bytes_to_send = snprintf(
-				buffer,
-				BUFFER_SIZE,
-				"GET / HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n",
-				Client::IP
-			);
+		static void out(const char* raw_request_data){
+			if (Client::socket_TCP == -1) return;
+			if (!raw_request_data) return Log::error("out(): null raw_request_data");
 
-			if (send(Client::socket_TCP, buffer, bytes_to_send, 0) < 0) Log::error("out(): Error");
+			// NOTE: The moment you need to send a body that may contain '\0' (compressed data, protobufs, etc.), this breaks.
+			size_t length = strlen(raw_request_data);
+			if (length == 0) return Log::error("out(): Empty request");
+
+			if (!Client::send_all_bytes(raw_request_data, length)) return;
+
+			Log::success("out(): request sent");
+		}
+
+		static bool send_all_bytes(const char* data, size_t length) {
+			while (length) {
+				ssize_t bytes_sent = ::send(Client::socket_TCP, data, length, 0);
+
+				if (bytes_sent < 0) {
+					Log::error("send_all(): send failed");
+					return false;
+				}
+
+				data += (size_t)bytes_sent;
+				length -= (size_t)bytes_sent;
+			}
+			return true;
 		}
 
 		static void in() {
